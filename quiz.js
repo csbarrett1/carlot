27b2e6e5717701f22bc0af3bed2938468463c271
Chris Barrett <c.barrett@me.com>
2016-04-30 13:54:36 -0500

    completed changes

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 "use strict"
 
-var CarLot = (function (cars) {
+var CarLot = (function() {
 
   // Loop over your array of cars and build up an HTML string to build a card for each car.
   // Use Bootstrap to create rows. Each row should contain 3 columns.
@@ -9,44 +9,50 @@ var CarLot = (function (cars) {
 
   function populatePage (inventory) {
     for (let i = 0; i < inventory.length; i++) {
-      carList.innerHTML += "<div class= 'col-xs-4'>" + "<div class='thumbnail'>" + "<img src=" + inventory[i].picture + ">" + "<div  class='info thinBorder' style= 'height: 190px; padding: 5px; margin-top: 5px; border-radius: 5px; border-color: " + inventory[i].borderColor + "'>" + `<h3 style='margin-top: 0px'> ${inventory[i].year}` + " " + inventory[i].make + " " + `${inventory[i].model}</h3>` + `<h5>${inventory[i].color}</h5>` + `<p>${inventory[i].description}</p>` + "</div>" + "</div>" + "</div>";
+      carList.innerHTML += "<div class= 'col-xs-4'>" + "<div class='thumbnail'>" + "<img src=" + inventory[i].picture + ">" + "<article  class='info normalState' style= 'height: 190px; padding: 5px; margin-top: 5px; border-radius: 5px; border-color: " + inventory[i].borderColor + "'>" + `<h3 style='margin-top: 0px'> ${inventory[i].year}` + " " + inventory[i].make + " " + `${inventory[i].model}</h3>` + `<h5>${inventory[i].color}</h5>` + `<p class="description" id="card-${i}">${inventory[i].description}</p>` + "</article>" + "</div>" + "</div>";
       };
   CarLot.loadInventory(activateEvents);
+  CarLot.loadInventory(reset);
     }
 
   // Now that the DOM is loaded, establish all the event listeners needed
   // The second IIFE should augment the original one with a function that creates all of the eventHandlers that you need for the application.
-
-  function activateEvents (inventory) {
-    var input = document.getElementById("userInput");
-    var info = document.getElementsByClassName("info");
-    var childrenEl = info.childNodes;
-    var childEl;
-
   // When you click on one of the car elements, change the width of the border to a higher value, and change the background color to any other color of your choosing.
 
-    for (let i = 0; i < inventory.length; i++) {
-      info[i].addEventListener("click", function(event) {
-        childEl = event.target.closest("div");
-        childEl.classList.toggle("border");
-        childEl.classList.add("background");
+  function activateEvents (inventory) {
+    var info = document.getElementsByClassName("info");
 
-  // On click of the car element, clear the value of the text input in the navbar, and put the cursor in the text input
+    $(document).on("click", function(event) {
+      var clickedOn = $("article");
+      clickedOn.removeClass("clickedOn");
+      $(event.target).closest("article").addClass("clickedOn");
+      $("input").focus();
+      var rando =  $(event.target).closest("p").attr('id');
+      description();
+    });
+  }
 
-        input.value = "";
-        input.focus();
-        description(childEl.childNodes[2]);
-      })
-    };
 
+  // // On click of the car element, clear the value of the text input in the navbar, and put the cursor in the text input
   // When you start typing into the navbar's text input, the description of the currently selected car should be bound to what you are typing in and match it exactly.
 
-    function description(swap) {
-      input.addEventListener("keyup", function(event) {
-        var text = input.value;
-        swap.innerHTML = text;
-      });
-    }
+  function description() {
+   $("input").keyup(function() {
+      var inputStuff = $("input").val();
+      $(".clickedOn").html(inputStuff);
+    });
+    $("input").val("");
+  }
+
+// One function resets the border thickness and background color for each car element back to the original values
+
+  function reset (inventory) {
+    var button = document.getElementById("button");
+    var info = document.getElementsByClassName("info");
+
+    $("#button").on("click", function(event) {
+      location.reload();
+    })
   }
 
   // Load the inventory and send a callback function to be invoked after the process is complete
